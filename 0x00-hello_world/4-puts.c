47156915e4612406e0093d7b4130d4a3ebecd974
Kishakul <roselineakaolisa@gmail.com>
2022-12-08 23:34:57 +0100

    Added Hello World files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - print the phrase 'Programming is like building a multilingual puzzle'
+* Return: 0 if quoted correctly, non zero otherwise
+*/
+
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
