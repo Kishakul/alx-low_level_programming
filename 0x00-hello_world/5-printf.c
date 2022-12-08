7e8b26b5288f315436e09899f8ad6e36e7c3c552
Kishakul <roselineakaolisa@gmail.com>
2022-12-09 00:21:37 +0100

    Added a printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints exactly this grammar,
+*with proper grammar, but the outcome is a piece of art,
+* Return: 0 if its prints properly, non-zero if otherwise
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (10);
+}
