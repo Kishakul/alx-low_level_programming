69d172b27fd00b9ffaeba8fbeb7e46819d8e1f77
Kishakul <roselineakaolisa@gmail.com>
2022-12-09 01:04:58 +0100

    modified some files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - print the phrase 'Programming is like building a multilingual puzzle'
+* main - print the phrase Programming is like building a multilingual puzzle
 * Return: 0 if quoted correctly, non zero otherwise
 */
 
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle\n");
+puts("programming is like building a multilingual puzzle\n");
 return (0);
 }
