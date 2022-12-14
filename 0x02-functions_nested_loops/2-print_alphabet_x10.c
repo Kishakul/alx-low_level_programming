07d90b68a6ccfad42543499c1ce8dd10eb63b920
Kishakul <roselineakaolisa@gmail.com>
2022-12-14 04:37:01 +0100

    Added functions and nested loops file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - entry point
+*Return: Always 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+int num;
+char ch;
+num = 0;
+for (num = 0; num <= 10; num++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+ch++;
+}
+}
+_putchar('\n');
+num++;
+}
