2d1a37156c4796f79ecf377c8d99bc004c7077f3
Kishakul <roselineakaolisa@gmail.com>
2022-12-14 05:36:58 +0100

    Addedna function and nested file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,11 +4,11 @@
 * _islower - code entry point
 * @c: The integer value it recieves
 * checks lowercase character
-* Return: Always 0
+* Return: 1 if c is lowercase, 0 otherwise
 */
 int _islower(int c)
 {
-int alpha = 'a'
+int alpha = 'a';
 for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 if (c == alpha)
