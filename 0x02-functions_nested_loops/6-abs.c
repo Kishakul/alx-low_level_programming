d973cd3acbe0c62e6dfec677b1da235808b63ff1
Kishakul <roselineakaolisa@gmail.com>
2022-12-14 15:03:53 +0100

    Added functions and nested loops file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _abs - functions that computes absolute value of an integer
+* @n: the int to check
+* Return: Always 0
+*/
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
