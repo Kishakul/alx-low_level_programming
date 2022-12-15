68e3c8a6bface5474ab6ee19be1236b3f72f5e58
Kishakul <roselineakaolisa@gmail.com>
2022-12-15 21:06:23 +0100

    Added file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
 * @c: character for checks
 * Return: 1 if c is uppercase, 0 if otherwise
 */
-int _isupper(int c);
+int _isupper(int c)
 {
-return ((c >= 'A' && c <= 'Z') ? (1) : (0));
+return (c >= 'A' && c <= 'Z');
 }
