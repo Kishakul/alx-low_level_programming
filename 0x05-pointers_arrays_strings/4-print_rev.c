7207e6cb0da42ce697cfc01fd7b16ef6d244b8eb
Kishakul <roselineakaolisa@gmail.com>
2022-12-19 12:52:35 +0100

    Added a pointer file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
-* rev_string - reverse a string
+* _rev - reverse a string
 * @s: string to reverse
-* Return: void
+* Return: string in reverse
 */
-void rev_string(char *s)
+void _rev(char *s)
 {
-int i = 0, j = 0;
-char str[500];
-while (*(s + i))
+int i = 0;
+while (s[i] != 0)
 {
-*(str + i) = *(s + i);
 i++;
 }
-i = i - 1;
-while (i >= 0)
+while (i > 0)
 {
-*(s + i) = *(str + j);
-j++;
 i--;
+_putchar(s[i]);
 }
+_putchar('\n');
 }
