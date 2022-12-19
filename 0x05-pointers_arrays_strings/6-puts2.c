a5c75a5a89f5519a9443e84dc9e7e7bddc07a427
Kishakul <roselineakaolisa@gmail.com>
2022-12-19 13:18:44 +0100

    Added a pointer file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of a string
+* @str: the string to print
+* Return: nothing
+*/
+void puts2(char *str)
+{
+int i, j;
+for (i = 0; str[i] != '\0'; i++)
+{
+;
+}
+for (j = 0; j < i; j = j + 2)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
+}
