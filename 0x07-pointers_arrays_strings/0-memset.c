3cce8e1647c7d64ce5e535ef5f045fcaa264466b
Kishakul <roselineakaolisa@gmail.com>
2023-01-10 00:28:40 +0100

    Added file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,12 +17,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-/**
-* declare an unsigned int
-* because we are storing a
-* value that will always be
-* non-negative (zero or positive)
-*/
+  /**
+   * declare an unsigned int
+   * because we are storing a
+   * value that will always be
+   * non-negative (zero or positive)
+  */
 unsigned int i;
 
 for (i = 0; i < n; i++)
