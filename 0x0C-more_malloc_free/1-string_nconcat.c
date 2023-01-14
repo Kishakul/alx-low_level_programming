c82c2caeaf980f7e2ced9df5a3fba1df65170b01
Kishakul <roselineakaolisa@gmail.com>
2023-01-14 03:45:12 +0100

    Added file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
 * string_nconcat - function that concatenates two strings
@@ -10,41 +11,40 @@
 * Return: new string followed by the first @n bytes
 *         of string 2 @s2 or NULL
 */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int 11, 12, x, y;
-char *str;
+char *ar;
+unsigned int i = 0;
+unsigned int j = 0;
+unsigned int m;
+unsigned int p;
+unsigned int k = 0;
+unsigned int len;
 
-  /*treat NULL as empty string*/
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-11 = 12 = 0;
-  /*length of both s1 & s2 respectively*/
-while (s1[11] != '\0')
-11++;
-while (s2[12] != '\0')
-12++;
+while (s1[i])
+i++;
+while (s2[j])
+j++;
 
-  /*set n to length of s2*/
-if (n >= 12)
-n = 12;
-str = (char *) malloc((10 + n + 1) * sizeof(char));
-if (str == NULL)
+if (j > n)
+j = n;
+
+len = i + j;
+
+ar = malloc(sizeof(char) * (len + 1));
+if (ar == NULL)
 return (NULL);
 
-  /*add si to str*/
-for (x = 0; s1[x] != '\0'; x++)
-str[x] = s1[x];
-  /*add s2 n bytes to str*/
-for (y = 0; y < n && s2[y] != '\0'; y++)
-{
-str[x] = s2[y];
-x++;
-}
-str[x] = '\0';
-return (str);
+for (p = 0; p < i; p++)
+ar[k++] = s1[p];
+for (m = 0; m < j; m++)
+ar[k++] = s2[m];
+
+ar[k] = '\0';
+return (ar);
 }
