770b9b00e7136dfdcc201e7060b9f53d46914c81
Kishakul <roselineakaolisa@gmail.com>
2023-01-17 20:44:58 +0100

    modified dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,18 +2,18 @@
 #define DOG_H
 
 /**
- * struct dog - dog structure
- *
- * @name: name of dog
- * @age: age of the dog
- * @owner: dog owner
- *
- */
+* struct dog - dog structure
+*
+* @name: name of dog
+* @age: age of the dog
+* @owner: dog owner
+*
+*/
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 /**
